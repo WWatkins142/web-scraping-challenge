1420f15ce95ea54ad0ca4e2be24541e349feec70
WWatkins142 <wesley.d.rainey@gmail.com>
2022-01-02 18:51:43 -0500

    Create app.py

---
+++ Missions_to_Mars/app.py
@@ -0,0 +1,18 @@
+# imports
+from flask import Flask, render_template, redirect, url_for
+from flask_pymongo import PyMongo
+import scrape_mars
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "You reached the index"
+
+@app.route("/scrape")
+def scrape():
+    return "You reached the scrape route"
+
+
+if __name__== "__main__":
+    app.run()
