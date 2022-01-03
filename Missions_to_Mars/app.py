289ec63a6f2e8fba363db588659b6475b06dd3fd
WWatkins142 <wesley.d.rainey@gmail.com>
2022-01-02 19:16:58 -0500

    Update app.py
    mongoDB connection

--- Missions_to_Mars/app.py
+++ Missions_to_Mars/app.py
@@ -5,14 +5,33 @@ import scrape_mars
 
 app = Flask(__name__)
 
+# use flask pymongo to set up connection to database
+app.config["MONGO_URI"] = "mongodb://localhost:27017/marsData_db"
+mongo = PyMongo(app)
+
 @app.route("/")
 def index():
-    return "You reached the index"
+    # access information from database
+    marsData = mongo.db.marsData.find_one()
+    print(marsData)
+    return "Flask app loaded"
 
 @app.route("/scrape")
 def scrape():
-    return "You reached the scrape route"
+    # reference to database collection
+    marsTable = mongo.db.marsData
 
+    # drop table if exists
+    mongo.db.marsData.drop()
+
+    # scrape mars script
+    marsData = scrape_mars.scrape_all()
+
+    #load dictionary into mongoDB
+    marsTable.insert_one(marsData)
+
+    # go back to index route
+    return redirect("/")
 
 if __name__== "__main__":
     app.run()
